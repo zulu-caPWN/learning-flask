2ecbd06e13d4c0dc5792966894102c5da4e28719
PK <penpester@gmail.com>
2018-05-22 04:27:37 -0700

    added password length and email format checks

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import Form 
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 class SignUpForm(Form):
     first_name = StringField('First name', validators=[DataRequired('Enter your first name')])
     last_name = StringField('Last name', validators=[DataRequired('Enter your last name')])
-    email = StringField('Email', validators=[DataRequired('Enter your email')])
-    password = PasswordField('Password', validators=[DataRequired('Enter a password')])
+    email = StringField('Email', validators=[DataRequired('Enter your email'), Email('U done goofed, enter a valid email')])
+    password = PasswordField('Password', validators=[DataRequired('Enter a password'), Length(min=6, message='Password must be 6 charachters minimum') ])
     submit = SubmitField('Sign Up')
